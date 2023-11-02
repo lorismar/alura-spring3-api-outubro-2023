8a023519aa037335c576d265d52d421c099cdcce
Lorismar <lorismar.rosendo@tjro.jus.br>
2023-11-02 09:37:07 -0400

    Implemntação de classes

---
+++ HELP.md
@@ -0,0 +1,7 @@
+# Getting Started
+
+### Reference Documentation
+For further reference, please consider the following sections:
+
+* https://trello.com/c/YwnK9c9m/1-cadastro-de-m%C3%A9dicos
+* https://www.figma.com/file/N4CgpJqsg7gjbKuDmra3EV/Voll.med?type=design&node-id=2-1007&mode=design
---
+++ src/main/java/med/voll/api/controller/MedicoContrtoller.java
@@ -0,0 +1,19 @@
+package med.voll.api.controller;
+
+
+import med.voll.api.medico.DadosCadastroMedico;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("medicos")
+public class MedicoContrtoller {
+
+
+    @PostMapping
+    public void cadastrar(@RequestBody DadosCadastroMedico dados){
+        System.out.println(dados);
+    }
+
+
+
+}
